3ba2d83b9666b4f56c35d1bd0fbc547ec04b7f74
OmarMahmoud2002 <omarmahmoud.devv@gmail.com>
2025-05-26 13:32:37 +0300

    fix controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
-class UsersController < ApplicationController::Base
-  def show
-    @users = "OSAD 45"
+class UsersController < ApplicationController
+  def index
+    @users = User.all
   end
-end
